c047cc60474be2a6419d144e0980f7bfd9833034
Kate Gamo <keito.gamo@gmail.com>
2019-02-10 18:36:16 +0900

    Initial Commit

---
+++ src/main/java/com/kate/aem/demo/App.java
@@ -0,0 +1,13 @@
+package com.kate.aem.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/kate/aem/demo/AppTest.java
@@ -0,0 +1,20 @@
+package com.kate.aem.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
